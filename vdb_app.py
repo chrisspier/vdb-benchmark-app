82a78618be7f512d6a40376b8fde5f7e29bae2fb
Octavian Zarzu <136714196+octavianz-y42@users.noreply.github.com>
2023-09-28 15:45:18 +0000

    commit

--- vdb_app.py
+++ vdb_app.py
@@ -2,11 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from PIL import Image
+import webbrowser
 
 
 
 # Function to calculate costs (same as the one I provided in the previous response)
-def calculate_costs(data_models, data_volume, code_changes, env_count, rollbacks, compute_to_storage_ratio):
+def calculate_costs(data_models, data_volume, code_changes, env_count, rollbacks, compute_to_storage_ratio, tooling_invest):
     # Constants
     storage_cost_per_tb = 23
     gb_to_tb = 1 / 1000
@@ -25,10 +26,10 @@ def calculate_costs(data_models, data_volume, code_changes, env_count, rollbacks
     
     # Calculate savings percentage
     saving_percentage = round(((total_cost_vdb_disabled - total_cost_vdb_enabled) / total_cost_vdb_disabled) * 100,2)
-    total_savings = total_cost_vdb_disabled * (saving_percentage / 100)
+    total_savings = total_cost_vdb_disabled-total_cost_vdb_enabled
 
     #Calculate ROI
-    roi = round(total_savings/tooling_invest)
+    roi = 100 * (total_savings-tooling_invest)/tooling_invest
 
     return {
         'Data Models': data_models,
@@ -54,12 +55,12 @@ def calculate_costs(data_models, data_volume, code_changes, env_count, rollbacks
 # Define the default values and presets
 # DEFAULT_VALUES = [200, 100, 40, 2, 1, 7]
 PRESET_VALUES = {
-    "X-Small": [50, 100, 15, 2, 1, 7],
-    "Small": [200, 100, 40, 2, 1, 7],
-    "Medium": [1000, 10000, 300, 3, 3, 7],
-    "Medium 2": [5000, 30000, 400, 4, 3, 8],
-    "Large": [3000, 100000, 600, 5, 5, 7],
-    "Large 2": [1500, 800000, 1000, 5, 30, 12]
+    "X-Small": [50, 100, 15, 2, 1, 7, 10],
+    "Small": [200, 100, 40, 2, 1, 7, 10],
+    "Medium": [1000, 10000, 300, 3, 3, 7, 100],
+    "Medium 2": [5000, 30000, 400, 4, 3, 8, 100],
+    "Large": [3000, 100000, 600, 5, 5, 7, 1000],
+    "Large 2": [1500, 800000, 1000, 5, 30, 12, 1000]
 }
 
 # Function to set team size to Custom
@@ -91,7 +92,7 @@ with st.expander("What are Virtual Data Builds?", expanded=False):
     - **Instant deployment and rollback of your code changes**. Your asset’s view definition can be updated to reference any previous materialization (from any branch). 
     - **Work on production data in your feature branch** without duplicating data or impacting the production environment/branch with no additional cost.
     - **Less mental overhead**: VDB converts your code operations into data warehouse assets, saving you the pain of managing environments and promoting code through. 
-    - **Significant savings on warehouse costs, ranging from 40% to 70%** depending on your setup, as we will see in this benchmark.
+    - **Significant savings on warehouse costs, ranging from 37% to 68%** depending on your setup, as we will see in this benchmark.
     
     You can find out more about Virtual Data Builds in [this article](https://www.y42.com/blog/virtual-data-builds-one-data-warehouse-environment-for-every-git-commit/)
     """)
@@ -106,29 +107,38 @@ col1, col2, col3, col4 = st.columns(4)
 
 # Now add your filters inside these columns
 with col1:
-    data_models = st.number_input('Data Models', value=200, step=100)  # Adjust the value and label accordingly
+    data_models = st.number_input('Data Models', value=1000, step=100)  # Adjust the value and label accordingly
 
 with col2:
-    data_volume = st.number_input('Data Volume (GB)', value=100, step=100)  # Adjust the value and label accordingly
+    data_volume = st.number_input('Data Volume (GB)', value=10000, step=100)  # Adjust the value and label accordingly
 
 with col3:
-    code_changes = st.number_input('Monthly Code Changes', value=40, step=100)  # Adjust the value and label accordingly
+    code_changes = st.number_input('Monthly Code Changes', value=300, step=100)  # Adjust the value and label accordingly
 
 with col4:
-    env_count = st.number_input('Environments', value=2, step=1)  # Adjust the value and label accordingly
+    env_count = st.number_input('Environments', value=3, step=1)  # Adjust the value and label accordingly
 
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    rollbacks = st.number_input('Rollbacks', value=1, step=1)  # Adjust the value and label accordingly
+    rollbacks = st.number_input('Rollbacks', value=3, step=1)  # Adjust the value and label accordingly
 
 with col2:
     compute_to_storage_ratio = st.number_input('Compute to Storage ratio', value=7, step=1)  # Adjust the value and label accordingly
 
 with col3:
-    tooling_invest = st.number_input('Tool Investment (per month)', value = 1000, step=100) # Adjust the value and label accordingly
+    tooling_invest = st.number_input('Tool Investment (per month)', value = 100, step=100) # Adjust the value and label accordingly
 
 # Initialize session state
+if 'roi' not in st.session_state:
+    st.session_state['roi'] = '910'
+
+if 'total_savings' not in st.session_state:
+    st.session_state['total_savings'] = '1010'
+
+if 'tooling_invest' not in st.session_state:
+    st.session_state['tooling_invest'] = '100'
+
 if 'df' not in st.session_state:
     # Initialize your DataFrame here with your predefined data
     small_values = calculate_costs(*PRESET_VALUES["X-Small"])
@@ -141,14 +151,19 @@ if 'df' not in st.session_state:
     st.session_state['df'] = pd.DataFrame([small_values, small_values_2, medium_values, medium_values_2, large_values, large_values_2])
 
 def add_entry(data_models, data_volume, code_changes, env_count, rollbacks, compute_to_storage_ratio):
-    new_entry = calculate_costs(data_models, data_volume, code_changes, env_count, rollbacks, compute_to_storage_ratio)
+    new_entry = calculate_costs(data_models, data_volume, code_changes, env_count, rollbacks, compute_to_storage_ratio, tooling_invest)
 
     # Ensuring the saving_percentage is not negative
     if new_entry['Savings %'] < 0:
         new_entry['Savings %'] = 0
-
+    
+    st.session_state['roi'] = new_entry['ROI']
+    st.session_state['total_savings'] = new_entry['Savings $']
+    st.session_state['tooling_invest'] = new_entry['Tool Investment (per month)']
 
     st.session_state['df'] = pd.concat([st.session_state['df'], pd.DataFrame([new_entry])], ignore_index=True)
+
+
 
 
 def remove_entry(index):
@@ -161,18 +176,33 @@ def remove_entry(index):
 if st.button("Add New Entry"):
     add_entry(data_models, data_volume, code_changes, env_count, rollbacks, compute_to_storage_ratio)
 
+
 st.markdown('---')
+
+st.markdown("### ROI Summary")
+st.write('Please note that these figures refer to the last entry added. Removing an entry will not impact these figures.')
+st.write('Tool investment: ', st.session_state['tooling_invest'], st.session_state['total_savings'], st.session_state['roi'])
 
 col1, col2, col3 = st.columns(3)
 
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:300px !important;
+    }
+    </style> st.session_state['tooling_invest']
+    """, unsafe_allow_html=True
+)
+
 with col1:
-   st.header(tooling_invest)
+    st.markdown(st.session_state['tooling_invest'])   
 
-with col2:
-  st.header(total_savings)
+# with col2:
+#     compute_to_storage_ratio = st.number_input('Compute to Storage ratio', value=7, step=1)  # Adjust the value and label accordingly
 
-with col3:
-   st.header(roi)
+# with col3:
+#     tooling_invest = st.number_input('Tool Investment (per month)', value = 100, step=100) # Adjust the value and label accordingly
+
 
 st.markdown('---')
 
@@ -193,6 +223,7 @@ with st.expander("Storage and Compute cost formulas", expanded=False):
     - **Storage cost** = Data Volume + <span style='color: green;'>New materializations as a result of code changes</span>
     """, unsafe_allow_html=True)
 
+
 st.dataframe(
     st.session_state['df'],
     column_config={
@@ -203,9 +234,9 @@ st.dataframe(
         "Total": st.column_config.NumberColumn(format="$%d"),
         "Total (VDB)": st.column_config.NumberColumn(format="$%d"),
         "Savings %": st.column_config.NumberColumn(format="%d%%"),
-        "Savings $": st.column_config.NumberColumn(format="$%d"),
-        "Tool Investment (per month)": st.column_config.NumberColumn(format="$%d"),
-        "ROI": st.column_config.NumberColumn(format="%d%%"),
+        # "Savings $": st.column_config.NumberColumn(format="$%d"),
+        # "Tool Investment (per month)": st.column_config.NumberColumn(format="$%d"),
+        # "ROI": st.column_config.NumberColumn(format="%d%%"),
     },
     hide_index=False,
 )
@@ -286,6 +317,9 @@ div.stButton > button:hover {
     }
 </style>""", unsafe_allow_html=True)
 
-b = st.button("Try Y42 for free and save DWH cost today")
+url = 'https://www.y42.com/form/call/form-lp-hs-form-on-homepage-get-free-trial/'
+
+if st.button("Try Y42 for free and save DWH cost today"):
+    webbrowser.open_new_tab(url)
 
 
